9791b6cde26706ee6603127dd209a05e7c6a5e63
Joane <joanealves2011@gmail.com>
2022-10-03 21:29:30 -0300

    first commit

---
+++ telas/service/cliente-service.js
@@ -0,0 +1,31 @@
+const criarNovaLinha = (nome, email) => {
+    const linhaNovoCliente = document.createElement('tr')
+    const conteudo = `
+    <td td class="td" data - td > ${nome}</td>
+    <td>${email}</td>
+                <td>
+                    <ul class="tabela__botoes-controle">
+                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                    </ul>
+                </td>
+    `
+
+    linhaNovoCliente.innerHTML = conteudo
+    return linhaNovoCliente
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
+const http = new XMLHttpRequest()
+
+http.open('Get', 'http://localhost:3000/profile')
+
+http.send()
+
+http.onload = () => {
+    const data = JSON.parse(http.response)
+    data.forEach(elemento => {
+        tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email))
+    })
+}
